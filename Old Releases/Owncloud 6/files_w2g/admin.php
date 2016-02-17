0bb5f7722728635724bdf6b7480128a081aef09c
Patt92 <hop@bito.de>
2016-02-17 14:50:59 +0100

    Removed
    removed for release management

--- Old Releases/Owncloud 6/files_w2g/admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OCA\files_w2g;
-
-\OCP\User::checkAdminUser();
-
-$tem = new \OCP\Template(app::name, 'admin');
-return $tem->fetchPage();
-
--- Old Releases/Owncloud 6/files_w2g/ajax/l10n.php
+++
@@ -1,3 +0,0 @@
-<?php
-$l = \OCP\Util::getL10N('files_w2g');
-if($l->t($_POST['rawtext'])!="") echo $l->t($_POST['rawtext']); else echo $_POST['rawtext'];
