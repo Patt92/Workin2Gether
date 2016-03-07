bfad1dcec86942945ee41ac52e481cb9ceeff3f0
Patt92 <hop@bito.de>
2016-03-07 14:17:51 +0100

    removed charset var

--- files_w2g/appinfo/app.php
+++ files_w2g/appinfo/app.php
@@ -12,8 +12,6 @@ class app{
 
 	const table = 'locks_w2g';
 
-	const charset = 'DEFAULT CHARACTER SET=utf8 DEFAULT COLLATE=utf8_bin';
-
 	public static function launch()
 	{
 
@@ -33,7 +31,6 @@ if (\OCP\App::isEnabled(app::name)) {
         if(!\OC::$server->getDatabaseConnection()->tableExists( app::table )){
 			try {
                 $db_exist = \OCP\DB::prepare("CREATE table *PREFIX*" . app::table . "(name varchar(255) PRIMARY KEY,created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,locked_by varchar(255))");
-				// " . app::charset
                 $db_exist->execute();
 			}catch(Exception $e) {	
 			}
