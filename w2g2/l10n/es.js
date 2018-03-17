3e6f879e221c149d2bf6f9e9a0bf50a74a3abe19
Ismail Bina <12769948+untilbit@users.noreply.github.com>
2018-03-17 16:12:35 +0100

    Spanish l10n files
    Spanish translation

---
+++ w2g2/l10n/es.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "w2g2", {
+        "File not locked": "Archivo no bloqueado",
+        "File is locked": "Archivo bloqueado",
+        "Status: locked": "Estado: bloqueado",
+        "Status: not locked": "Estado: no bloqueado",
+        "filelock": "filelock",
+        "You locked the file": "Has bloqueado el archivo"
+    },
+    "nplurals=2; plural=(n != 1);");
